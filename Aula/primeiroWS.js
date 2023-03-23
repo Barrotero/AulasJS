d12274d56f4f2b26ec847ebc2a1582aa8121a391
David Carvalho de Oliveira <davidcarvalho_2@outlook.com>
2023-03-23 11:52:16 -0300

    Aula 1

---
+++ Aula/primeiroWS.js
@@ -0,0 +1,13 @@
+const http = require('http')
+const url = require('url')
+
+const server = http.createServer((req, res) => {
+    res.writeHead(200, {'Content-Type' : 'text/plain'})
+
+    let queryData = url.parse(req.url, true).query
+    if(queryData.name)
+        res.end(`Oi, ${queryData.name}`)
+    else
+        res.end('Oi, my consagrated!') 
+}).listen(8080)
+
